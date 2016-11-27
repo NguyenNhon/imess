6a86031c356d120ce7de8e5933e030644cca0898
Nhơn Nguyễn <ngthinhon1995@gmail.com>
2016-11-27 16:16:08 +0700

    Add view group.

---
+++ imess/imess/AddFriendInGroup.swift
@@ -0,0 +1,24 @@
+//
+//  AddFriendInGroup.swift
+//  imess
+//
+//  Created by Nhon Nguyen on 11/27/16.
+//  Copyright © 2016 Nhon Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class AddFriendInGroup: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+}
--- imess/imess/AddGroupViewController.swift
+++ imess/imess/AddGroupViewController.swift
@@ -8,9 +8,28 @@
 
 import UIKit
 
-class AddGroupViewController: UIViewController {
+class AddGroupViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    @IBOutlet weak var tfGroupName: UITextField!
+    @IBOutlet weak var imgGroupPicture: UIImageView!
+    @IBOutlet weak var tvFriends: UITableView!
+    @IBAction func addFriendClick(_ sender: Any) {
+        let addFriendInGroup = self.storyboard?.instantiateViewController(withIdentifier: "addFriendInGroup")
+        self.navigationController?.pushViewController(addFriendInGroup!, animated: true)
+    }
+    
+    let friendsOfCurrentUser : [User] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationController?.isNavigationBarHidden = false
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return friendsOfCurrentUser.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
 }
